79fe6c0c7db7a2de247253019afdfac431f823e1
Khoi Do <khoizpro31@gmail.com>
2024-02-15 00:50:58 +0700

    Add Favicon and title

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -1,4 +1,10 @@
+import { Metadata } from "next";
 import { LoginForm } from "./_components/login-form";
+
+export const metadata: Metadata = {
+  title: "Login",
+  description: "Login to access our application",
+};
 
 const Login = () => {
   return (
